8ac856cd40a91e4e267dcdf5f6845cebf133a2f3
Bano-Barry <barrymamadoubano14@gmail.com>
2024-06-14 17:29:03 +0000

    P2C3 - Reactions aux évenements envoyés au template

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -8,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrl: './face-snap.component.scss'
 })
 export class FaceSnapComponent {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  snapButtonText! : string;
+  userHasSnapped! : boolean;
 
+  ngOnInit() : void {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis toujours !';
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+    onSnap(): void {
+      if (this.userHasSnapped) {
+        this.unSnap();
+      } else {
+        this.snap();
+      }
+    }
+
+    unSnap() {
+        this.snaps--;
+        this.snapButtonText = 'Oh Snap!';
+        this.userHasSnapped = false;
+    }
+
+    snap() {
+        this.snaps++;
+        this.snapButtonText = 'Oops, unSnap!';
+        this.userHasSnapped = true;
+    }
 }
