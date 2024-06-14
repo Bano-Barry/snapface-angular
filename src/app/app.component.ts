5bab90f0299028a4ad0eae56bb1594cf3c27c5ea
Bano-Barry <barrymamadoubano14@gmail.com>
2024-06-14 15:52:38 +0000

    Création du premier component de l'app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    FaceSnapComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
